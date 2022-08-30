ea15c454130002e49b3c4849c2ef5f0ebf4b0e37
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-30 13:13:10 +0900

    s

--- routes/web.php
+++ routes/web.php
@@ -60,3 +60,4 @@ Route::get('/stamp','App\Http\Controllers\stamp@index')-> name('stamp');
 
 //公式アカウントへイベントを送信send
 Route::get('/send','App\Http\Controllers\SendEvents@index')-> name('send.index');
+Route::post('/send','App\Http\Controllers\SendEvents@send')-> name('send.send');
