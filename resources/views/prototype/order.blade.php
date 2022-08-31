19f0a3c18b6b25384695583bb43f5ce0fe6efa8c
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-31 13:56:14 +0900

    s

--- resources/views/prototype/order.blade.php
+++ resources/views/prototype/order.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ secure_asset('css/proOrder.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/proOrder.scss') }}">
     <meta name="viewport"
    content="width=320,
       height=480,
