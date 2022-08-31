3ead4446c6988cdaa2f4fa18c21a8ed62dc7c991
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-31 13:12:19 +0900

    HTML用意

---
+++ resources/views/prototype/order.blade.php
@@ -0,0 +1,19 @@
+<header class="site-header">
+    <h1 class="site-logo"><img src="images/logo.png" alt="WEBDESIGNDAY"></h1>
+    <nav class="gnav">
+        <ul class="gnav__menu">
+            <li class="gnav__menu__item"><a href="">About</a></li>
+            <li class="gnav__menu__item"><a href="">Works</a></li>
+            <li class="gnav__menu__item"><a href="">Recruit</a></li>
+            <li class="gnav__menu__item"><a href="">News</a></li>
+            <li class="gnav__menu__item"><a href="">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+<div class="hero"><img src="images/hero.jpg" alt="hero"></div>
+<div class="content">
+    <!--省略-->
+</div>
+<footer class="site-footer">
+    <p class="copyright">@2017 WEBDESIGNDAY</p>
+</footer>
