363adc346320afe7e129ec021aadf0f99a862837
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-31 13:27:46 +0900

    rinnkumei

--- resources/views/entrance.blade.php
+++ resources/views/entrance.blade.php
@@ -38,7 +38,7 @@
     <a href="https://lin.ee/mjCgvBo">発行中のクーポン</a>
     <a href="https://line-logins.herokuapp.com/gotoauthpage">line_login</a>
     <a href="{{route('send.index')}}">公式アカウントの操作</a>
-    <a href="{{route('porder.index')}}">公式アカウントの操作</a>
+    <a href="{{route('porder.index')}}">protoOrder</a>
 
     <div class="note">
 
