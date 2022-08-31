4eb2bd69dc30ae2d7ae925c81f50be7f95814420
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-31 17:13:20 +0900

    ああああああ

--- resources/views/send/sendevent.blade.php
+++ resources/views/send/sendevent.blade.php
@@ -5,10 +5,10 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="{{ secure_asset('css/lineDetail.css') }}">  
-    {{--  <link rel="stylesheet" href="{{ secure_asset('css/reset.css') }}">    --}}
-    {{--  <link id="import-link" rel="import" href="./sub.html">  --}}
-    <script src="https://unpkg.com/glottologist"></script> 
+    <link rel="stylesheet" href="{{ secure_asset('css/lineDetail.css') }}">
+    {{-- <link rel="stylesheet" href="{{ secure_asset('css/reset.css') }}"> --}}
+    {{-- <link id="import-link" rel="import" href="./sub.html"> --}}
+    <script src="https://unpkg.com/glottologist"></script>
     <title>WELCOME</title>
 </head>
 
@@ -17,9 +17,9 @@
     <h1>SendMessage</h1>
 
     @if (isset($rtn))
-    <p> {{$rtn}}</p>
+        <p> {{ $rtn }}</p>
     @endif
-  
+
     <p>ようこそ！ <span id="displayNameField"> </span> さん</p>
 
     <p>入力したメッセージを 公式アカウントからあなたへ送信します</p>
@@ -28,60 +28,63 @@
     <form method="POST">
         @csrf
 
-   {{--  <p> <input type="text" name="msg"> </p>
-   <p> <input type="text" name="msg2"> </p>  --}}
-   <input type="hidden" id="userIdProps" value="" name="id">
-   <div class="line-bc">
+        {{-- <p> <input type="text" name="msg"> </p>
+   <p> <input type="text" name="msg2"> </p> --}}
+        <input type="hidden" id="userIdProps" value="" name="id">
+        <div class="line-bc">
 
-    <div class="balloon6">
-      <div class="faceicon">
-        <img src="{{ secure_asset('img/default.png') }}" alt="def">
-      </div>
+            <div class="balloon6">
+                <div class="faceicon">
+                    <img src="{{ secure_asset('img/default.png') }}" alt="def">
+                </div>
 
-      <div class="chatting">
-        <div class="says">
-          <p> <input type="text" name="msg" required></p>
-        </div>
-      </div>
-      <td></td>
-      <div class="chatting">
-        <div class="says">
-          <p> <input type="text" name="msg2" required></p>
-        </div>
+                <div class="chatting">
+                    <div class="says">
+                        <p> <input type="text" name="msg" required></p>
+                    </div>
+                </div>
+                <td></td>
+                <div class="chatting">
+                    <div class="says">
+                        <p> <input type="text" name="msg2" required></p>
+                    </div>
+                </div>
+
+                <div class="mycomment">
+                    <p>
+                        
+                    </p>
+
+                </div>
+            </div>
+    </form>
     </div>
-
-    <div class="mycomment">
-      <p>
-       　　　　　
-      </p>
-      
+    <div align="center">
+        <a href="{{ route('send.send') }}"></a>
+        <button type="submit">自分に送信</button>
     </div>
-  </div>
-</form>
-</div>
-<div align="center">
-<a href="{{route ('send.send')}}"></a>
-<button type="submit">自分に送信</button>
-</div>
-<p></p>
-
-<div bgcolor="#c0c0c0" margin="2">
-    <p>＊解説＊  </p>
-    <p>公式アカウントへイベントを発信し、入力を取得して操作したユーザへ向けたメッセージを送信しています </p>
-    <p>通常はトークルーム内から行われるイベントの発信を、WEB画面から行っています。</p>
-    <p>ブロードキャスト配信やターゲティング配信も可能です(未実装)</p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <div bgcolor="#c0c0c0" margin="2">
+        <p>＊解説＊ </p>
+        <p>公式アカウントへイベントを発信し、入力を取得して操作したユーザへ向けたメッセージを送信しています </p>
+        <p>通常はトークルーム内から行われるイベントの発信を、WEB画面から行っています。</p>
+        <p>様々な命令が出せるので、ブロードキャスト配信やターゲティング配信も可能です(未実装)</p>
+        <p>パラメータを付けてイベントを発信しているので、トークルーム内のユーザはこの操作が出来ません</p>
     </div>
     <div class="note">
 
         <p glot-model="note1"></p>
         <p glot-model="note2"></p>
-    
+
     </div>
 
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
     <script src="js/jquery-1.9.0.min.js" type="text/javascript"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-   <script src="js/getuserid.js"></script> 
+    <script src="js/getuserid.js"></script>
 </body>
 
 </html>
