2b7fdb5940b68f6d5ae410356ba876ea7c7b28d0
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-31 14:20:20 +0900

    s

--- resources/views/prototype/order.blade.php
+++ resources/views/prototype/order.blade.php
@@ -47,7 +47,7 @@
 
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
     <script src="js/jquery-1.9.0.min.js" type="text/javascript"></script>
-    <script src="js/protoOrder.js"></script>
+    {{--  <script src="js/protoOrder.js"></script>  --}}
 </body>
 <footer class="site-footer">
     <p class="copyright">@2017 WEBDESIGNDAY</p>
