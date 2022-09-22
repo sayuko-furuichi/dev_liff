d5d7e56ea30ab5a49d205c799ec897eb3fde1499
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-22 16:05:59 +0900

    あ

--- public/js/home.js
+++ public/js/home.js
@@ -5,7 +5,7 @@ const cloudfrontUrl = 'https://dev-liff.herokuapp.com/public/'
 
 window.onload = function () {
   const useNodeJS = false;   // if you are not using a node server, set this value to false
-  const defaultLiffId = '1657181787-2vrnwwlj';   // change the default LIFF value if you are not using a node server
+  const defaultLiffId = '1657487333-JPeEg6lr';   // change the default LIFF value if you are not using a node server
 
   //ペーじが出来上がったら、liffIDを渡してinitさせる
   $(document).ready(function () {
