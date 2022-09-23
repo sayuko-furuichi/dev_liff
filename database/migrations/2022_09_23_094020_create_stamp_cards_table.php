a06687211bb64ee3dfc1a19e18fa8c8ed8650885
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-23 09:48:14 +0900

    0923 migration_files

---
+++ database/migrations/2022_09_23_094020_create_stamp_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stamp_cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('lineuser_id');
+            $table->string('img');
+            $table->string('store_id');
+            $table->string('namber');
+            $table->string('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stamp_cards');
+    }
+};
