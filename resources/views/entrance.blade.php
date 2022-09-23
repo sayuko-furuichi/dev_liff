fac92817bb32ca2b1494dfc46a534f4250d5f473
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-23 16:02:20 +0900

    syouryaku

--- resources/views/entrance.blade.php
+++ resources/views/entrance.blade.php
@@ -12,40 +12,6 @@
     <title>WELCOME</title>
 </head>
 
-<body bgcolor="#c0c0c0">
-
-    <h1>home</h1>
-    <p>Hello!!! world!!!! LINEMINI APP</p>
-
-    <p>ようこそ！ <span id="displayNameField"> </span> さん</p>
-    
-
-   <form method="get"> 
-       @csrf 
-     <button type="submit" href="{{ route('getuser.index') }}">user</button>
-      </form> 
-  
-    <a href="{{route('getuser.index') }}">ユーザデータ取得</a>
-
-    <a href="https://line-logins.herokuapp.com/gotoauthpage">line_login</a>
-    <a href="{{route('send.index')}}">公式アカウントの操作</a>
-
-    <div class="note">
-
-        <p glot-model="note1"></p>
-        <p glot-model="note2"></p>
-    </div>
-    <div id="profileInfo">
-        <div id="profilePictureDiv" class="profile-picture" ></div>  
-        <div class="profile-info">
-            <h1 glot-model="companyName"></h1>
-            <p glot-model="departmentName"></p>
-            <p id="displayNameField"></p>
-            <p id="userIdProfileField"></p>
-            <p id="statusMessageField"></p>
-        </div>
-    </div>
-
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
     <script src="js/jquery-1.9.0.min.js" type="text/javascript"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
