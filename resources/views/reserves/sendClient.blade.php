613aeee9d8e74bcbd71d22dba899e01769e2b1fc
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-23 15:05:20 +0900

    ちょっと変更

--- resources/views/reserves/sendClient.blade.php
+++ resources/views/reserves/sendClient.blade.php
@@ -13,7 +13,7 @@
 </head>
 {{--  会員登録後、特別な表示と会員証画面へリダイレクトさせる  --}}
 <body>
-    <a href="{{route('member.mypage')}}">マイページ（サンプル用）</a>
+    <a href="{{route('reserve.submit')}}">完了画面へ</a>
     <div class="note">
     {{-- name属性つける --}}
     <form action="{{route('reserve.submit')}}" method="POST">
