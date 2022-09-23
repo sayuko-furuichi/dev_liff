7fca58fd8c395e0ce262d87d38e66f1c30da031b
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-23 16:05:39 +0900

    s

--- resources/views/entrance.blade.php
+++ resources/views/entrance.blade.php
@@ -5,16 +5,10 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     {{--  <link rel="stylesheet" href="{{ secure_asset('css/liff.css') }}">    --}}
-    {{--  <link rel="stylesheet" href="{{ secure_asset('css/reset.css') }}">    --}}
-    {{--  <link id="import-link" rel="import" href="./sub.html">  --}}
-    <script src="https://unpkg.com/glottologist"></script> 
-    <title>WELCOME</title>
+    <title>now loading...</title>
 </head>
-
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
     <script src="js/jquery-1.9.0.min.js" type="text/javascript"></script>
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="js/home.js"></script>
 </body>
 
