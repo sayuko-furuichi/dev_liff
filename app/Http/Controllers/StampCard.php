4c22ae419886a9eb314768ce0b2a54763c1c62a1
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-23 11:17:19 +0900

    OK

--- app/Http/Controllers/StampCard.php
+++ app/Http/Controllers/StampCard.php
@@ -10,6 +10,7 @@ class StampCard extends Controller
     function index(){
 
         //カードの作成から
+        //liff.init()後にuserIDが取得できるので、その時にカード取得判定が出来る
     
         return view('stampCards.stampCard');
 
