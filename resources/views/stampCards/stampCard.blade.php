48938ad988579bca1e9e5da1516c6be1f4cb9c3c
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-23 16:41:46 +0900

    これイルカ？

--- resources/views/stampCards/stampCard.blade.php
+++ resources/views/stampCards/stampCard.blade.php
@@ -40,15 +40,15 @@
       
         </div>
       
-        <div class='r_wrap'>
+        {{--  <div class='r_wrap'>  --}}
       
           {{--  <div class='b_round'></div>  --}}
-          <div class='s_round'>
+          {{--  <div class='s_round'>  --}}
             {{--  <div class='s_arrow'></div>  --}}
-          </div>
+          {{--  </div>
         </div>
       
-      </div>
+      </div>  --}}
 
     <div class="return">
     </div>
