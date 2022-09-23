7b75abd37fd3fafb4d9e7333446d7e66c2850af8
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-23 16:06:09 +0900

    a

--- resources/views/entrance.blade.php
+++ resources/views/entrance.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>now loading...</title>
 </head>
+<body>
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
     <script src="js/jquery-1.9.0.min.js" type="text/javascript"></script>
     <script src="js/home.js"></script>
