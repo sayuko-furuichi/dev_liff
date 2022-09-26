b16cf00121260c71e3dbff09880a3f10777b1844
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-26 13:32:27 +0900

    img追加（一応）

--- resources/views/reserves/sendClient.blade.php
+++ resources/views/reserves/sendClient.blade.php
@@ -13,7 +13,7 @@
 </head>
 {{--  会員登録後、特別な表示と会員証画面へリダイレクトさせる  --}}
 <body>
-    <a href="{{route('reserve.submit')}}">完了画面へ</a>
+    <img src="{{secure_asset('img/menu_bar/var_3.png')}}" alt="ながれ">
     <div class="note">
     {{-- name属性つける --}}
     <form action="{{route('reserve.submit')}}" method="POST">
