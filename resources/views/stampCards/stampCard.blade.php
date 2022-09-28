d82a13989f686ddd5d196b1e64d60e29d239e5aa
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-28 10:56:16 +0900

    s

--- resources/views/stampCards/stampCard.blade.php
+++ resources/views/stampCards/stampCard.blade.php
@@ -59,8 +59,9 @@
         <button type="button" id="qr" class="bild_qr_btn">GET！<img src="{{ secure_asset('img/qr_syu.png') }}"
                 alt="qrイメージ" class="qr_img"></button>
     </div>
+    <div >
     <p> 読み取り結果：<span id="stamp"></span></p>
-
+</div>
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
     <script src="js/jquery-1.9.0.min.js" type="text/javascript"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
