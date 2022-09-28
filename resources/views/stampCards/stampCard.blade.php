7be97414ee0065c290c83c11e199267eeaeddd90
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-28 18:05:56 +0900

    s

--- resources/views/stampCards/stampCard.blade.php
+++ resources/views/stampCards/stampCard.blade.php
@@ -67,7 +67,7 @@
             <p class="result"><button id="stamp" class="stamps" type="submit"></button></p>
             <p class="get_points"> ポイント総数：<span id="points" class="get_points"></span></p>
             @if (isset($getpoint))
-                <p>{{$getpoint}} ポイントゲットしました！！</p>
+                <p class="getpoints">{{$getpoint}} ポイントゲットしました！！</p>
             @endif
             {{--  <input type="submit" value="更新">  --}}
         </div>
