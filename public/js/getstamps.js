e5cb9c9a1ce2b4bf44872a059d5284eabf24bb56
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-28 18:35:44 +0900

    s

--- public/js/getstamps.js
+++ public/js/getstamps.js
@@ -20,7 +20,7 @@ var s_round = '.s_round';
     document.getElementById("qr").onclick= function(){
       liff.scanCodeV2().then(function (string) {
        
-        if(string.value != null || string.value != 'undefined'){
+        if(string != null || string != 'undefined'){
         window.location.href = string.value + '&user=' + document.getElementById('user_id').value;
       }
     //     var str =string.value;
