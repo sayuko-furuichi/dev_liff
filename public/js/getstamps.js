360ed509472d00beae465556db765bda1d3595b1
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-28 14:34:36 +0900

    ss

--- public/js/getstamps.js
+++ public/js/getstamps.js
@@ -21,7 +21,9 @@ var s_round = '.s_round';
       liff.scanCodeV2().then(function (string) {
           document.getElementById('stamp').value=string.value;
           document.getElementById('stamp').textContent=string.value;
-          document.getElementById('forms').action=string.value;
+       //   document.getElementById('forms').action=string.value;
+          document.forms.action=string.value;
+    
 
           var p =new RegExp('points=','gi'); 
           var strs =string.value.match(p);
