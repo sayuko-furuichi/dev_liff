926edebc9e53ea7503fe7f764f70c75200574f04
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-28 15:04:16 +0900

    s

--- resources/views/stampCards/stampCard.blade.php
+++ resources/views/stampCards/stampCard.blade.php
@@ -5,7 +5,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{--  <link rel="stylesheet" href="{{ secure_asset('css/membersCard.css') }}">  --}}
+    <link rel="stylesheet" href="{{ secure_asset('css/membersCard.css') }}">
     {{-- <link rel="stylesheet" href="{{ secure_asset('css/reset.css') }}"> --}}
     {{-- <link id="import-link" rel="import" href="./sub.html"> --}}
     <script src="https://unpkg.com/glottologist"></script>
