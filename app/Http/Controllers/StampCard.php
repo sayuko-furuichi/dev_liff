6352080a27859ca323e97d061f4a44d49b85cac0
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-29 12:43:10 +0900

    s

--- app/Http/Controllers/StampCard.php
+++ app/Http/Controllers/StampCard.php
@@ -17,9 +17,9 @@ class StampCard extends Controller
     }
     function add(Request $request){
 
-        // if(isset($request->points)){
-
-        // }
+         if(!isset($request)){
+            return redirect('/stamps');
+         }
 
       //  return view('stampCards.stampCard',['request'=> $request]);
     
