578340b2ff2add149a38101b05d5e5dc72b99b1f
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-29 17:13:16 +0900

    違う

--- public/js/getstamps.js
+++ public/js/getstamps.js
@@ -33,24 +33,24 @@ var s_round = '.s_round';
 
 
   
-  // $('.flip_box').hover(function() {
-  //   $('.b_round').toggleClass('b_round_hover');
-  //   return false;
-  // });
+  $('.flip_box').hover(function() {
+    $('.b_round').toggleClass('b_round_hover');
+    return false;
+  });
 
-  // $('.flip_box').click(function() {
-  //   $('.flip_box').toggleClass('flipped');
-  //   $(this).addClass('s_round_click');
-  //   // $('.s_arrow').toggleClass('s_arrow_rotate');
-  //   // $('.b_round').toggleClass('b_round_back_hover');
-  //   return false;
-  // });
+  $('.flip_box').click(function() {
+    $('.flip_box').toggleClass('flipped');
+    $(this).addClass('s_round_click');
+    // $('.s_arrow').toggleClass('s_arrow_rotate');
+    // $('.b_round').toggleClass('b_round_back_hover');
+    return false;
+  });
 
-  // $('.flip_box').on('transitionend', function() {
-  //   $(this).removeClass('s_round_click');
-  //   $(this).addClass('s_round_back');
-  //   return false;
-  // });
+  $('.flip_box').on('transitionend', function() {
+    $(this).removeClass('s_round_click');
+    $(this).addClass('s_round_back');
+    return false;
+  });
 });
   
   }
