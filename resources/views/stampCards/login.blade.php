9694c60f626e478b9aedb43f05c8889cadaff6d4
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-29 15:14:35 +0900

    s

--- resources/views/stampCards/login.blade.php
+++ resources/views/stampCards/login.blade.php
@@ -10,6 +10,6 @@
     <div id="store" value="{{$request->store}}"></div>
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
     <script src="js/login.js"></script>
-
+    <script src="js/jquery-1.9.0.min.js" type="text/javascript"></script>
 </body>
 </html>
