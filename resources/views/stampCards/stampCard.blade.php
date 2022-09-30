4bcbbb66fd6f87cbcd91a72201eccbce1ea9c605
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-30 10:51:59 +0900

    ss

--- resources/views/stampCards/stampCard.blade.php
+++ resources/views/stampCards/stampCard.blade.php
@@ -40,8 +40,8 @@
                     {{--  <img src="{{ secure_asset('img/1.png') }}" alt="logo" class="b_logo">  --}}
                     {{--  <h1 class='b_headline'>まる屋</h1>  --}}
                     <span>CARD NUMBER: </span><p class="b_text" id="card_no">{{$card_no}}</p>
-                    <p class="b_text"><span>STORE ID: </span>{{$store_id}}</p>
-                    <p class="b_text"><span>LINE USER ID: </span>{{$uid}}</p>
+                    <span>STORE ID: </span> <p class="b_text">{{$store_id}}</p>
+                    <span>LINE USER ID: </span><p class="b_text">{{$uid}}</p>
                     {{--  <p class='b_text'><br />text TEXT テキスト てきすと text TEXT テキスト てきすと<br />text TEXT テキスト てきすと</p>  --}}
                     {{--  <button class='b_button'><span>Buy now</span></button>  --}}
                 </div>
