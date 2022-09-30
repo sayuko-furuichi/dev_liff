1695b27fa8ed4a845d7f44b40cf1d08f97059cee
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-30 13:30:51 +0900

    とりあえず画面だけ返す

---
+++ app/Http/Controllers/Coupons.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Coupons extends Controller
+{
+    //
+    function index(){
+
+        return view('coupon_sample.44.benefits_4');
+    }
+
+    function used(){
+        
+    }
+}
