a75fefffbd5728ddcbea2601f7219a4a8457c039
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-30 14:55:48 +0900

    form送信

--- app/Http/Controllers/Coupons.php
+++ app/Http/Controllers/Coupons.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 class Coupons extends Controller
 {
     //
-    function index(){
+    function index(Request $request){
 
-        return view('coupon_sample.44.benefits_4');
+        return view('coupon_sample.44.benefits_4',['request'=>$request]);
     }
 
     function used(Request $request){
