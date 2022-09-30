7268bfef64bf936af548133f9c141ef0d8b60f71
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-30 18:35:43 +0900

    ss

--- resources/views/stampCards/stampCard.blade.php
+++ resources/views/stampCards/stampCard.blade.php
@@ -83,11 +83,12 @@
     <h2 class="bene_title">ポイントで獲得できる特典</h2>
     <form action="{{ route('coupon.index') }}" method="GET">
         @foreach ($cps as $cp)
-        @if ($points >= $cp->term_of_use_point)
-        <input type="hidden" name="couponId" value="{{ $cp->id }}">
-        <input type="submit" value="使用できます！" class="submit_btn">
-    @endif
+        
             <div class="benefits">
+                @if ($points >= $cp->term_of_use_point)
+        <input type="hidden" name="couponId" value="{{ $cp->id }}">
+        <input type="submit" value="GET！" class="submit_btn">
+    @endif
                 <div class="point_mark">{{$cp->term_of_use_point}}</div>
                 <img src="{{ secure_asset('img/1.png') }}" alt="img" class="bene_img">
                
