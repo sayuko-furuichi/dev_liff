de1b19f0d6f1182be957299dbbeed2c2d2e0dae4
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-30 16:52:58 +0900

    coupon_mst Model追加

---
+++ app/Models/CouponMst.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CouponMst extends Model
+{
+    use HasFactory;
+   protected $table='coupon_mst';
+}
