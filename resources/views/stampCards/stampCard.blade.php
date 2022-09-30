14ba98f0a1e1782c14bee5610068670e9aeb97f1
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-30 11:23:00 +0900

    ssss

--- resources/views/stampCards/stampCard.blade.php
+++ resources/views/stampCards/stampCard.blade.php
@@ -31,7 +31,7 @@
                         <img src="{{ secure_asset('img/stamps/card_8.svg') }}" alt="スタンプ枠" class="stamp_line">
                         {{--  <p class='f_title'>stamp CARD</p>  --}}
                     </div>
-                    <p class="limit">！ 有効期限：{{$expiry}}</p>
+                    <p class="limit">！ 有効期限：{{ $expiry }}</p>
                     <p class='f_subline'></p>
 
                 </div>
@@ -39,9 +39,12 @@
                 <div class='back'>
                     {{--  <img src="{{ secure_asset('img/1.png') }}" alt="logo" class="b_logo">  --}}
                     {{--  <h1 class='b_headline'>まる屋</h1>  --}}
-                    <span class='b_text'>CARD NUMBER: </span><p class="b_text" id="card_no">{{$card_no}}</p>
-                    <span class='b_text'>STORE ID: </span> <p class="b_text">{{$store_id}}</p>
-                    <span class='b_text'>LINE USER ID: </span><p class="b_text">{{$uid}}</p>
+                    <span class='b_text'>CARD NUMBER: </span>
+                    <p class="b_text" id="card_no">{{ $card_no }}</p>
+                    <span class='b_text'>STORE ID: </span>
+                    <p class="b_text">{{ $store_id }}</p>
+                    <span class='b_text'>LINE USER ID: </span>
+                    <p class="b_text">{{ $uid }}</p>
                     {{--  <p class='b_text'><br />text TEXT テキスト てきすと text TEXT テキスト てきすと<br />text TEXT テキスト てきすと</p>  --}}
                     {{--  <button class='b_button'><span>Buy now</span></button>  --}}
                 </div>
@@ -53,13 +56,13 @@
             {{--  <div class='b_round'></div>  --}}
             {{--  <div class='s_round'>  --}}
             {{--  <div class='s_arrow'></div>  --}}
-            {{--  </div>--}}
+            {{--  </div> --}}
         </div>
-      
-     
+
+
     </div>
-            <div class="return">
-            </div>
+    <div class="return">
+    </div>
 
     <div class="qr_btn">
         <button type="button" id="qr" class="bild_qr_btn">GET！<img src="{{ secure_asset('img/qr_syu.png') }}"
@@ -67,11 +70,10 @@
     </div>
     <form method="GET" id="forms">
         <div class="read_text">
-        
-            <p class="get_points"> ポイント総数：{{$points}}<span id="points" class="get_points"></span></p>
+
+            <p class="get_points"> ポイント総数：{{ $points }}<span id="points" class="get_points"></span></p>
             @if (isset($getPoints))
-         
-                <p class="get_points">{{$getPoints}} ポイントゲットしました！！</p>
+                <p class="get_points">{{ $getPoints }} ポイントゲットしました！！</p>
             @endif
             {{--  <input type="submit" value="更新">  --}}
         </div>
@@ -79,11 +81,27 @@
         <p>{{$uid}}</p>  --}}
         <input type="hidden" id="user_id" name="uid">
     </form>
-    <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
+    <div class="benefits">
+        <h2 class="bene_title">ポイントで獲得できる特典</h2>
+        <div class="bene_item">
+            <h3>4ポイント</h3>
+            <img src="{{ secure_asset('img/1.png') }}" alt="img">
+            <h4>お好きなドリンク 1杯無料！</h4>
+            <p>テキスト　テキスト</p>
 
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="{{secure_asset('js/getstamps.js')}}"></script>
-    <script src="{{secure_asset('js/jquery-1.9.0.min.js')}}" type="text/javascript"></script>
+        </div>
+        <div class="bene_item">
+            <h3>8ポイント<h3>
+                    <img src="{{ secure_asset('img/1.png') }}" alt="img">
+                    <h4>お好きなそば 1杯無料！</h4>
+                    <p>テキスト　テキスト</p>
+
+        </div>
+        <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
+
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+        <script src="{{ secure_asset('js/getstamps.js') }}"></script>
+        <script src="{{ secure_asset('js/jquery-1.9.0.min.js') }}" type="text/javascript"></script>
 </body>
 
 </html>
