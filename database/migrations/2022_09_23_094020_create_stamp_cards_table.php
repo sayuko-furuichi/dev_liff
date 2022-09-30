3cc9e7a610637e846390d280068599c027d13344
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-30 13:03:24 +0900

    a

--- database/migrations/2022_09_23_094020_create_stamp_cards_table.php
+++ database/migrations/2022_09_23_094020_create_stamp_cards_table.php
@@ -18,10 +18,11 @@ return new class extends Migration
             $table->string('lineuser_id');
             $table->string('img');
             $table->string('store_id');
-            $table->string('number');
+            $table->string('number')->comment('何枚目の発行か');
             $table->integer('state');
-            $table->timestamp('expiry')->nullable(true);
-            $table->integer('points');
+            $table->timestamp('expiry')->nullable(true)->comment('有効期限');
+            $table->integer('points')->comment('total_points');
+            $table->integer('max_points');
             $table->timestamps();
         });
     }
