f018b4af616e0e669fbde5f571f6424ed8f2f628
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-30 14:03:58 +0900

    s

--- resources/views/coupon_sample/44/benefits_4.blade.php
+++ resources/views/coupon_sample/44/benefits_4.blade.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{secure_asset('coupon.css')}}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>coupon</title>
 </head>
 <body>
-    <div>
-        <img src="{{secure_asset('img/coupon/coupon.svg')}}" alt="クーポン">
-        <img src="{{secure_asset('img/coupon/coupon.svg')}}" alt="クーポン">
+    <div class="coupons">
+        <img src="{{secure_asset('img/coupon/coupon.svg')}}" alt="クーポン" class="coupon_img">
+        <img src="{{secure_asset('img/coupon/used.svg')}}" alt="used" class="used_img">
     </div>
 
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
