9ad7ca5b812e22ab8601606dce3a5aa5c53c259d
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-03 17:45:01 +0900

    s

--- resources/views/coupon_sample/index.blade.php
+++ resources/views/coupon_sample/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{secure_asset('css/couponIndex.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('css/couponIndex.css') }}">
     <title>獲得した特典クーポン</title>
 </head>
 
@@ -14,47 +14,42 @@
 
         @if (isset($notFound))
             <p>使用できるクーポンがありません</p>
-            <a href="{{route('stamps.login')}}">ショップカードに戻る</a>
+            <a href="{{ route('stamps.login') }}">ショップカードに戻る</a>
         @else
-        <form action="{{ route('coupon.view') }}" method="GET">
-        <h2 class="bene_title">あなたが利用できるクーポン</h2>
-       
-        @if (isset($cps[0]))
-       
-        @foreach ($cps as $cp)
+            <form action="{{ route('coupon.view') }}" method="GET">
+                <h2 class="bene_title">あなたが利用できるクーポン</h2>
 
-       
-        <div class="benefits">
-            <div class="point_mark">{{$cp['term_of_use_point']}}</div>
-            <input type="hidden" name="couponId" value="{{ $cp['id'] }}">
-            {{--  <input type="submit" value="GET！" class="submit_btn">  --}}
-            <img src="{{ secure_asset($cp['img']) }}" alt="img" class="bene_img">
-            <h4>{{ $cp['name'] }}</h4>
-            <p>{{$cp['detail']}}</p>
-        </div>
+                @if (isset($cps[0]))
+
+                    @foreach ($cps as $cp)
+                        <div class="benefits">
+                            <div class="point_mark">{{ $cp['term_of_use_point'] }}</div>
+                            <input type="hidden" name="couponId" value="{{ $cp['id'] }}">
+                            {{--  <input type="submit" value="GET！" class="submit_btn">  --}}
+                            <img src="{{ secure_asset($cp['img']) }}" alt="img" class="bene_img">
+                            <h4>{{ $cp['name'] }}</h4>
+                            <p>{{ $cp['detail'] }}</p>
+                        </div>
+                    @endforeach
+                @else
+                    <div class="benefits">
+                        <div class="point_mark">{{ $cps['term_of_use_point'] }}</div>
+                        <input type="hidden" name="couponId" value="{{ $cps['id'] }}">
+                        <input type="hidden" name="cp" value="{{ $cps['img'] }}">
+                        {{--  <input type="submit" value="GET！" class="submit_btn">  --}}
+                        <img src="{{ secure_asset($cps['img']) }}" alt="img" class="bene_img">
+                        <h4>{{ $cps['name'] }}</h4>
+                        <p>{{ $cps['detail'] }}</p>
+                    </div>
+                @endif
+
+
+                <input type="hidden" id="userIdProps" name="uid">
+                <input type="hidden" name="store" value="{{ $store }}">
+                <input type="submit" value="（仮）">
+            </form>
+        @endif
     
-    @endforeach
-
-            @else
-          
-            <div class="benefits">
-                <div class="point_mark">{{$cps['term_of_use_point']}}</div>
-                <input type="hidden" name="couponId" value="{{ $cps['id'] }}">
-                <input type="hidden" name="cp" value="{{ $cps['img']}}">
-                {{--  <input type="submit" value="GET！" class="submit_btn">  --}}
-                <img src="{{ secure_asset($cps['img']) }}" alt="img" class="bene_img">
-                <h4>{{ $cps['name'] }}</h4>
-                <p>{{$cps['detail']}}</p>
-            </div>
-        @endif
-     
-        
-            <input type="hidden" id="userIdProps" name="uid">
-            <input type="hidden" name="store" value="{{$store}}">
-            <input type="submit" value="（仮）">
-      
-        @endif
-    </form>
     </div>
 
 
