cbc12869be945ce1b95cd50759083971e1c4e36f
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-03 11:54:13 +0900

    OK

--- resources/views/coupon_sample/index.blade.php
+++ resources/views/coupon_sample/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{secure_asset('css/couponIndex.css')}}">
     <title>獲得した特典クーポン</title>
 </head>
 
