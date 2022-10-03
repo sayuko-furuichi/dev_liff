455fc2712e15557c2386f5d6a30ca80c7b2512f6
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-03 15:51:56 +0900

    ss

--- resources/views/coupon_sample/44/benefits_4.blade.php
+++ resources/views/coupon_sample/44/benefits_4.blade.php
@@ -19,9 +19,10 @@
             @csrf
             <input type="hidden" name="user" value="{{$request->user}}">
             <input type="hidden" name="store" value="{{$request->store}}">
-       <input type="hidden" name="couponId" value="{{$request->couponId}}"> <img src="{{secure_asset('img/coupons/coupon.svg')}}" alt="クーポン" class="coupon_img">
+       <input type="hidden" name="couponId" value="{{$request->couponId}}"> 
+       {{--  <img src="{{secure_asset('img/coupons/coupon.svg')}}" alt="クーポン" class="coupon_img">  --}}
             @if (session('used'))
-           <img src="{{secure_asset('img/coupons/usedOn.svg')}}" alt="" class="used_img"> 
+           {{--  <img src="{{secure_asset('img/coupons/usedOn.svg')}}" alt="" class="used_img">   --}}
             @else
             <button type="submit"><img src="{{secure_asset('img/coupons/used.svg')}}" alt="used" class="used_img"></button>
             @endif
