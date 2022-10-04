4f018608a084db7cb8df1a316bfe9cb6c3799219
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-04 13:22:11 +0900

    s

--- resources/views/coupon_sample/index.blade.php
+++ resources/views/coupon_sample/index.blade.php
@@ -23,7 +23,7 @@
                 @if (isset($cps[0]))
 
                     @foreach ($cps as $cp)
-                    <input type="radio"  class="radio_btn">
+                    <input type="radio"  name="bene" class="radio_btn">
                         <div class="benefits">
                             <div class="point_mark">{{ $cp['term_of_use_point'] }}</div>
                             <input type="hidden" name="couponId" value="{{ $cp['id'] }}">
