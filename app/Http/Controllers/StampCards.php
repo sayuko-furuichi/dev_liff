f8190bdc346f569191e13b2adf44374ecc351050
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-04 17:50:06 +0900

    s

--- app/Http/Controllers/StampCards.php
+++ app/Http/Controllers/StampCards.php
@@ -42,7 +42,7 @@ class StampCards extends Controller
       //UserIdとstore_idをrequestに保持している状態
         //1：カードの持ち主を特定する
         $card =StampCard::where('lineuser_id', $request->userId)->get();
-    $card=  $card->where('store_id', $request->store)->sortByDesc('number');
+    $card=  $card->where('store_id', $request->store)->sortByDesc('id');
         
       
 
