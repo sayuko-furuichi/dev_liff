8d27d72dac8a72d3c4fb03b3a1d7d00d6d01a47b
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-04 09:24:09 +0900

    login

--- public/js/login.js
+++ public/js/login.js
@@ -1,31 +1,25 @@
-
 window.onload = function () {
-    const defaultLiffId = '1657487333-JPeEg6lr';   // change the default LIFF value if you are not using a node server
-
-    //ペーじが出来上がったら、liffIDを渡してinitさせる
-    $(document).ready(function () {
-      initializeLiff(defaultLiffId);
-    });
-  }
-  function initializeLiff(myLiffId) {
+  const defaultLiffId = '1657487333-JPeEg6lr';   // change the default LIFF value if you are not using a node server
+var req;
+  //ペーじが出来上がったら、liffIDを渡してinitさせる
+  $(document).ready(function () {
     liff
-      .init({
-        liffId: myLiffId
-      })
-      .then(() => {
-        if (!liff.isLoggedIn()) {
-          liff.login();
-        } else {
-  // req.open('POST','/',true)
-  // req.setRequestHeader('');
-        //   liff.getProfile().then(function(prof){
-        //     document.getElementById('store').textContent=prof.userId;
-            
-            liff.getProfile().then(function(prof){
+    .init({
+      liffId: defaultLiffId
+    })
+    .then(() => {
+      if (!liff.isLoggedIn()) {
+        liff.login();
+
+      }else{
+        liff.getProfile().then(function(prof){
               
-            //location.hrefにuserIdのクエリをつける
-            // LIFFURLを使用すると、何度もredirectするので注意
-            window.location.href ='https://dev-liff.herokuapp.com/public/stamps/index?userId=' + prof.userId + '&store='+ document.getElementById('store').value;
-  
-          })
-        }},)}
+          //location.hrefにuserIdのクエリをつける
+          // LIFFURLを使用すると、何度もredirectするので注意
+          window.location.href ='https://dev-liff.herokuapp.com/public/stamps/index?userId=' + prof.userId + '&store='+ document.getElementById('store').value;
+
+      })
+    }}) .catch(function (error) {
+        window.alert('エラーです' + error);});}
+   
+  );}
