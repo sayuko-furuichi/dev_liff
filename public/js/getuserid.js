a1e4b90ca1879b2234951695292cfbf218066f3f
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-04 11:07:03 +0900

    d

--- public/js/getuserid.js
+++ public/js/getuserid.js
@@ -1,54 +1,35 @@
-
-
-//多分、いちいちinitしなくていい。
-
-//イベントを使ってる。表示と値の取得の順番の為に書く
 window.onload = function () {
-	const useNodeJS = false;   // if you are not using a node server, set this value to false
-	// store_tableから、LIFFIDを持ってくる？
-//	const defaultLiffId = '1657181787-2vrnwwlj';   // change the default LIFF value if you are not using a node server
-//softnext　のLIFFIDでログイン
-const defaultLiffId ='1657487333-JPeEg6lr';
- 
-const cloudfrontUrl = 'https://dev-liff.herokuapp.com/public/';
-
-    //jQueryを使い、DOMの読み込みが完了したときに処理を実行
+	const defaultLiffId = '1657487333-JPeEg6lr';   // change the default LIFF value if you are not using a node server
+  
+	//ペーじが出来上がったら、liffIDを渡してinitさせる
 	$(document).ready(function () {
-		initializeLiff(defaultLiffId);
-	});
-}
-
-function initializeLiff(myLiffId) {
-	liff
-		.init({
-			liffId: myLiffId
+	  liff
+	  .init({
+		liffId: defaultLiffId
+	  })
+	  .then(() => {
+		if (!liff.isLoggedIn()) {
+		  liff.login();
+  
+		}else{
+		  liff.getProfile().then(function(prof){
+				
+			document.getElementById('user').value=prof.userId;
+  
 		})
-		.then(() => {
-			if (!liff.isLoggedIn()) {
-				liff.login();
-			} else {
+	  }}) .catch(function (error) {
+		  window.alert('エラーです' + error);});}
+	 
+	);}
 
-
-  liff.getProfile().then(function (profile) {
-
-
-		document.getElementById('userIdProps').value = profile.userId;
-	
-
-
-}).catch(function (error) {
-    window.alert('Error getting profile: ' + error);
-  });
-    
-
-function getParam(name, url) {
-//表示ページのURLを取得
-if (!url) url = window.location.href;
-name = name.replace(/[\[\]]/g, "\\$&").replace(/[()]/g, "");
-var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-results = regex.exec(url);
-if (!results) return null;
-if (!results[2]) return '';
-return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-  }})}
+// function getParam(name, url) {
+// //表示ページのURLを取得
+// if (!url) url = window.location.href;
+// name = name.replace(/[\[\]]/g, "\\$&").replace(/[()]/g, "");
+// var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+// results = regex.exec(url);
+// if (!results) return null;
+// if (!results[2]) return '';
+// return decodeURIComponent(results[2].replace(/\+/g, " "));
+// }
+//   }})}
