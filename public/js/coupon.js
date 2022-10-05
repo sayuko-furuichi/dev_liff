6da1c8dd955aee84e07c68a42f97c3a50a15c72d
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-05 10:53:00 +0900

    s

--- public/js/coupon.js
+++ public/js/coupon.js
@@ -1,22 +1,23 @@
 window.onload = function () {
-  const defaultLiffId = '1657487333-JPeEg6lr';   // change the default LIFF value if you are not using a node server
-
-  //ペーじが出来上がったら、liffIDを渡してinitさせる
-  $(document).ready(function () {
-    liff
-    .init({
-      liffId: defaultLiffId
-    })
-    .then(() => {
-      if (!liff.isLoggedIn()) {
-        liff.login();
-
-      }else{
-        liff.getProfile().then(function(prof){
-          document.getElementById('userId').value=prof.userId;
-          document.getElementById('user').value=prof.userId;
-      })
-    }}) .catch(function (error) {
-        window.alert('エラー :' + error);});}
-   
-  );}
+	const defaultLiffId = '1657487333-JPeEg6lr';   // change the default LIFF value if you are not using a node server
+  
+	//ペーじが出来上がったら、liffIDを渡してinitさせる
+	$(document).ready(function () {
+	  liff
+	  .init({
+		liffId: defaultLiffId
+	  })
+	  .then(() => {
+		if (!liff.isLoggedIn()) {
+		  liff.login();
+  
+		}else{
+		  liff.getProfile().then(function(prof){
+				
+			document.getElementById('user').value=prof.userId;
+  
+		})
+	  }}) .catch(function (error) {
+		  window.alert('エラー :' + error);});}
+	 
+	);}
