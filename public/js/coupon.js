30026963b010c74c072b033d259a577f7b923e76
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-05 11:38:56 +0900

    a

--- public/js/coupon.js
+++ public/js/coupon.js
@@ -2,6 +2,7 @@ window.onload = function () {
 	const defaultLiffId = '1657487333-JPeEg6lr';   // change the default LIFF value if you are not using a node server
   liffInit(defaultLiffId);
 
+
 	//ペーじが出来上がったら、liffIDを渡してinitさせる
 	function liffInit(defaultLiffId){
    $(document).ready(function () {
@@ -16,6 +17,7 @@ window.onload = function () {
 		  liff.getProfile().then(function(prof){
 				
 			document.getElementById('user').value=prof.userId;
+      document.getElementById('users').textContent=prof.userId;
       
     })
 	  }}) .catch(function (error) {
--- public/js/getuserid.js
+++ public/js/getuserid.js
@@ -1,7 +1,6 @@
 window.onload = function () {
 	const defaultLiffId = '1657487333-JPeEg6lr';   // change the default LIFF value if you are not using a node server
   liffInit(defaultLiffId);
-},
 
 	//ペーじが出来上がったら、liffIDを渡してinitさせる
 	function liffInit(defaultLiffId){
@@ -13,14 +12,12 @@ window.onload = function () {
 	  .then(() => {
 		if (!liff.isLoggedIn()) {
 		  liff.login();
-  
 		}else{
 		  liff.getProfile().then(function(prof){
 				
 			document.getElementById('user').value=prof.userId;
-			document.getElementById('users').textContent=prof.userId;
       
     })
 	  }}) .catch(function (error) {
 		  window.alert('エラー :' + error);});}
-   );}
+   );}}
