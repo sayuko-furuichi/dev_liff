adcd24d6362b3435c24a5910d364774b38de8c55
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-17 13:44:27 +0900

    あ

--- app/Http/Controllers/Members.php
+++ app/Http/Controllers/Members.php
@@ -22,8 +22,8 @@ class Members extends Controller
         $nwClient->line_user_id = $request->userId;
         $nwClient->first_name = $request->mei;
         $nwClient->last_name = $request->sei;
-        $nwClient->mei = $request->fmei;
-        $nwClient->sei = $request->fsei;
+        $nwClient->mei = $request->fMei;
+        $nwClient->sei = $request->fSei;
         $nwClient->phone_number = $request->phone_number;
         $nwClient->prefecture_id = $request->prefecture_id;
         $nwClient->email = $request->email;
