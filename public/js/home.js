146a4fe2f4aade9297f7cd3728c4de20bf433036
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-17 18:52:21 +0900

    a

--- public/js/home.js
+++ public/js/home.js
@@ -1,24 +1,20 @@
 
 window.onload = function () {
   const defaultLiffId = '1657487333-JPeEg6lr';   // change the default LIFF value if you are not using a node server
-liffInit(defaultLiffId);
-
-
-  function liffInit(defaultLiffId){
-  //https://liff.line.me/1657487333-wakMRydO
-
+var req;
   //ペーじが出来上がったら、liffIDを渡してinitさせる
-  document.ready(
-    liff.init({
+  $(document).ready(function () {
+    liff
+    .init({
       liffId: defaultLiffId
     })
     .then(() => {
       if (!liff.isLoggedIn()) {
         liff.login();
       }}) .catch(function (error) {
-        window.alert('エラーです' + error);}));}
+        window.alert('エラーです' + error);});}
    
-  ;}
+  );}
 
 
  
