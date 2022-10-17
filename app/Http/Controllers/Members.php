bb766dee84a3e07635c9565953bfac51dcd64f71
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-17 13:43:23 +0900

    line_user_id　が取れてなかった

--- app/Http/Controllers/Members.php
+++ app/Http/Controllers/Members.php
@@ -9,38 +9,41 @@ use App\Models\Prefecture;
 class Members extends Controller
 {
     //
-    function index(Request $request){
+    public function index(Request $request)
+    {
         $prefectutes= Prefecture::all();
 
-        return view('members.addMember',['prefecture'=>$prefectutes ,'store_id'=>$request->store]);
+        return view('members.addMember', ['prefecture'=>$prefectutes ,'store_id'=>$request->store]);
     }
 
-    function add(Request $request){
-        $nwClient = new Client;
-       $nwClient->line_user_id = $request->userId;
-       $nwClient->first_name = $request->mei;
-       $nwClient->last_name = $request->sei;
-       $nwClient->mei = $request->fmei;
-       $nwClient->sei = $request->fsei;
-       $nwClient->phone_number = $request->phone_number;
-       $nwClient->prefecture_id = $request->prefecture_id;
-       $nwClient->email = $request->email;
-       $nwClient->municipality = $request->municipality;
-       $nwClient->house_number = $request->house_number;
+    public function add(Request $request)
+    {
+        $nwClient = new Client();
+        $nwClient->line_user_id = $request->userId;
+        $nwClient->first_name = $request->mei;
+        $nwClient->last_name = $request->sei;
+        $nwClient->mei = $request->fmei;
+        $nwClient->sei = $request->fsei;
+        $nwClient->phone_number = $request->phone_number;
+        $nwClient->prefecture_id = $request->prefecture_id;
+        $nwClient->email = $request->email;
+        $nwClient->municipality = $request->municipality;
+        $nwClient->house_number = $request->house_number;
+        $nwClient->line_user_id = $request->userId;
+        $nwClient->building_name ='';
+        $nwClient->store_id = $request->store_id;
+        $nwClient->c_corporate_number='';
+        $nwClient->c_corporate_name='';
+        $nwClient->referral_number ='';
 
-       $nwClient->building_name ='';
-       $nwClient->store_id = $request->store_id;
-       $nwClient->c_corporate_number='';
-       $nwClient->c_corporate_name='';
-       $nwClient->referral_number ='';
-
-     $nwClient->save();
+        $nwClient->save();
 
 
-        return redirect('/addMember')->with('result','登録が完了しました！');
+        return redirect('/addMember')->with('result', '登録が完了しました！');
     }
 
-    function myPage(){
+    public function myPage()
+    {
         return view('members.mypage');
     }
 }
