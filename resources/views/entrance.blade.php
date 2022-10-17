3f989262c5f471094fef197bd38cea7218fe1eb6
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-17 18:14:34 +0900

    a

--- resources/views/entrance.blade.php
+++ resources/views/entrance.blade.php
@@ -10,7 +10,7 @@
 <body>
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
     <script src="js/jquery-1.9.0.min.js" type="text/javascript"></script>
-    <script src="js/home.js"></script>
+    <script src="{{secure_asset('js/home.js')}}"></script>
 </body>
 
 </html>
