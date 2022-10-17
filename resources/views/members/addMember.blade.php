d0f320189fc7acc1d9552d5e25ad55bcc7f0d694
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-17 11:44:45 +0900

    住所入力欄変更

--- resources/views/members/addMember.blade.php
+++ resources/views/members/addMember.blade.php
@@ -33,7 +33,9 @@
     </div>
     <div  class="item">
         <p class="sub_title">　住所　　<span  class="must">必須</span></p>
-        <input type="text" placeholder=""  required name="address" class="txt">
+        {{--  <input type="text" placeholder=""  required name="address" class="txt">  --}}
+        <input type="text" placeholder="都道府県"required name="pre" class="onethird_txt">　<input type="text" placeholder="市区町村"
+        required name="mei" class="onethird_txt">　 <input type="text" placeholder="番地" required name="番地" class="onethird_txt">
     </div>
     <div  class="item">
         <p class="sub_title">　メールアドレス　　<span class="must">必須</span></p>
