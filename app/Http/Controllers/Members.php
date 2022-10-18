cf97c4c22675e569a96e0914effce184d82cb018
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-18 14:54:01 +0900

    会員登録、入力のバリデーション

--- app/Http/Controllers/Members.php
+++ app/Http/Controllers/Members.php
@@ -19,114 +19,163 @@ class Members extends Controller
 
     public function add(Request $request)
     {
-       $old= Client::where('line_user_id',$request->userId)->where('store_id',$request->store_id)->first('id');
-        if(isset($old->id)){
-            return redirect('/addMember')->with('result', 'あなたはすでに会員です');
-        }else{
-            $nwClient = new Client();
-            $nwClient->line_user_id = $request->userId;
-            $nwClient->first_name = $request->mei;
-            $nwClient->last_name = $request->sei;
-            $nwClient->mei = $request->fMei;
-            $nwClient->sei = $request->fSei;
-            $nwClient->phone_number = $request->tel;
-            $nwClient->prefecture_id = $request->prefecture_id;
-            $nwClient->email = $request->email;
-            $nwClient->municipality = $request->municipality;
-            $nwClient->house_number = $request->house_number;
-            $nwClient->line_user_id = $request->userId;
-            $nwClient->store_id = $request->store_id;
+        $old= Client::where('line_user_id', $request->userId)->where('store_id', $request->store_id)->first();
+        if (!isset($old->id)) {
+          $nwClient = new Client();
+          $nwClient->line_user_id = $request->userId;
+          $nwClient->first_name = $request->mei;
+          $nwClient->last_name = $request->sei;
+          $nwClient->mei = $request->fMei;
+          $nwClient->sei = $request->fSei;
+          $nwClient->phone_number = $request->tel;
+          $nwClient->prefecture_id = $request->prefecture_id;
+          $nwClient->email = $request->email;
+          $nwClient->municipality = $request->municipality;
+          $nwClient->house_number = $request->house_number;
+          $nwClient->line_user_id = $request->userId;
+          $nwClient->store_id = $request->store_id;
 
-            $nwClient->building_name ='';          
-            $nwClient->c_corporate_number=0;
-            $nwClient->c_corporate_name='';
-            $nwClient->referral_number =0;
-            $nwClient->c_corporate_name='';
-    
+          $nwClient->building_name ='';
+          $nwClient->c_corporate_number=0;
+          $nwClient->c_corporate_name='';
+          $nwClient->referral_number =0;
+          $nwClient->c_corporate_name='';
+
+          //1018 追加
+          $nwClient->gender_id= 4;
+          $nwClient->cellular_phone= '';
+          $nwClient->sns1= '';
+          $nwClient->sns2= '';
+          $nwClient->sns3= '';
+          $nwClient->job= '';
+          $nwClient->allergy= '';
+          $nwClient->note= '';
+          $nwClient->business_card_id= 0;
+          $nwClient->number_of_visit= 0;
+          $nwClient->amount_of_payment= 0;
+          $nwClient->average_payment_amount= 0;
+          $nwClient->zip= '';
+
+          //ランク会員に設定する
+          $nwClient->member_attribute_id=2;
+
+
+          $nwClient->save();
+
+          //JSONでBotにpostする
+
+          //requestBody 後でjson_encodeします
+
+        $this->sendToBot($request->userId,$request->store_id);
+
+          return redirect('/addMember')->with('result', '登録が完了しました！');
+         
+        } elseif ($old->member_attribute_id==1) {
+            $old->line_user_id = $request->userId;
+            $old->first_name = $request->mei;
+            $old->last_name = $request->sei;
+            $old->mei = $request->fMei;
+            $old->sei = $request->fSei;
+            $old->phone_number = $request->tel;
+            $old->prefecture_id = $request->prefecture_id;
+            $old->email = $request->email;
+            $old->municipality = $request->municipality;
+            $old->house_number = $request->house_number;
+            $old->line_user_id = $request->userId;
+            $old->store_id = $request->store_id;
+
+            $old->building_name ='';
+            $old->c_corporate_number=0;
+            $old->c_corporate_name='';
+            $old->referral_number =0;
+            $old->c_corporate_name='';
+
             //1018 追加
-            $nwClient->gender_id= 4;
-            $nwClient->cellular_phone= '';
-            $nwClient->sns1= '';
-            $nwClient->sns2= '';
-            $nwClient->sns3= '';
-            $nwClient->job= '';
-            $nwClient->allergy= '';
-            $nwClient->note= '';
-            $nwClient->business_card_id= 0;
-            $nwClient->number_of_visit= 0;
-            $nwClient->amount_of_payment= 0;
-            $nwClient->average_payment_amount= 0;
-            $nwClient->zip= '';
-            
-            //ランク会員に設定する
-            $nwClient->member_attribute_id=2;
+            $old->gender_id= 4;
+            $old->cellular_phone= '';
+            $old->sns1= '';
+            $old->sns2= '';
+            $old->sns3= '';
+            $old->job= '';
+            $old->allergy= '';
+            $old->note= '';
+            $old->business_card_id= 0;
+            $old->number_of_visit= 0;
+            $old->amount_of_payment= 0;
+            $old->average_payment_amount= 0;
+            $old->zip= '';
+
+            //ランク会員に変更する
+            $old->member_attribute_id=2;
 
 
-            $nwClient->save();
-    
-            //JSONでBotにpostする
-    
-            //requestBody 後でjson_encodeします
-            $detail=([
-                        'events'=> [
-                          [
-                            'type'=> 'message',
-                            'message'=> [
-                              'type'=> 'message',
-                              'text'=>  '完了',
-                              //text2 に line_user_idをつける
-                              'userId'=>  $request->userId,
-                              
-                          ],
-                            'timestamp'=> $_SERVER['REQUEST_TIME'],
-                            'source'=> [
-                              'type'=> 'web',
-                            ],
-                            'mode'=> 'active',
-                            'deliveryContext'=> [
-                              'isRedelivery'=> false
-                            ]
-                          ],
-            
-                        ]
-                      ]);
-            
-            //チャネルアクセストークンを秘密鍵として、requestBodyをハッシュ化する
-            #アカウントのCSを取得する
-            $lineStore =lineStoreStatus::where('store_id',$request->store_id)->first('channel_secret');
-            
-            
-            $encode=json_encode($detail);
-            
-            $hash = hash_hmac('sha256', $encode, $lineStore->channel_secret, true);
-            $signature = base64_encode($hash);
-            
-            //ハッシュ化したものを[x_demo_signature]としてheaderにつける
-            $header = array(
-                'Content-Type: application/json',
-              'x_demo_signature:'. $signature,
-            );
-            
-            //JSON形式でPOST送信する
-            $context = stream_context_create([
-                'http' => [
-                    'ignore_errors' => true,
-                    'method' => 'POST',
-                    'header' => implode("\r\n", $header),
-                    'content' => $encode
-                ],
-            ]);
-            
-            //Botが起動するURLへPostする。 store_idをクエリで付けてください
-            $res=  file_get_contents('https://dev1.softnext.co.jp/syokusapo/linebot/public/api/callback?store_id='.$request->store_id, false, $context);
-            
-    
-    
-    
-            return redirect('/addMember')->with('result', '登録が完了しました！');
+            $old->save();
+
+            $this->sendToBot($request->userId,$request->store_id);
+            return redirect('/addMember')->with('result', '再登録が完了しました！');
+
+        } else {
+          return redirect('/addMember')->with('result', 'あなたはすでに会員です');
         }
-    
     }
+
+    public function sendToBot($userId,$storeId)
+    {
+        $detail=([
+          'events'=> [
+            [
+              'type'=> 'message',
+              'message'=> [
+                'type'=> 'message',
+                'text'=>  '完了',
+                //text2 に line_user_idをつける
+                'userId'=>  $userId,
+
+            ],
+              'timestamp'=> $_SERVER['REQUEST_TIME'],
+              'source'=> [
+                'type'=> 'web',
+              ],
+              'mode'=> 'active',
+              'deliveryContext'=> [
+                'isRedelivery'=> false
+              ]
+            ],
+
+          ]
+        ]);
+
+        //チャネルアクセストークンを秘密鍵として、requestBodyをハッシュ化する
+        #アカウントのCSを取得する
+        $lineStore =lineStoreStatus::where('store_id', $storeId)->first('channel_secret');
+
+
+        $encode=json_encode($detail);
+
+        $hash = hash_hmac('sha256', $encode, $lineStore->channel_secret, true);
+        $signature = base64_encode($hash);
+
+        //ハッシュ化したものを[x_demo_signature]としてheaderにつける
+        $header = array(
+        'Content-Type: application/json',
+        'x_demo_signature:'. $signature,
+        );
+
+        //JSON形式でPOST送信する
+        $context = stream_context_create([
+        'http' => [
+            'ignore_errors' => true,
+            'method' => 'POST',
+            'header' => implode("\r\n", $header),
+            'content' => $encode
+        ],
+        ]);
+
+        //Botが起動するURLへPostする。 store_idをクエリで付けてください
+        $res=  file_get_contents('https://dev1.softnext.co.jp/syokusapo/linebot/public/api/callback?store_id='.$storeId, false, $context);
+        return $res;
+    }
+
 
     public function myPage()
     {
--- resources/views/members/addMember.blade.php
+++ resources/views/members/addMember.blade.php
@@ -12,54 +12,56 @@
     <title>会員登録画面</title>
 </head>
 {{--  会員登録後、特別な表示と会員証画面へリダイレクトさせる  --}}
+
 <body>
     <div class="note">
-        @if(session('result'))
-    <p>{{session('result')}} 会員画面に遷移します</p>
+        @if (session('result'))
+            <p>{{ session('result') }} 会員画面に遷移します</p>
         @endif
-    {{-- name属性つける --}}
-    <form action="{{route('member.add')}}" method="POST">
-        @csrf
-    <div class="item">
-        <p class="sub_title">　名前　　<span  class="must">必須</span></p>
-        <input type="text" placeholder="姓"required name="sei" class="half_txt">　<input type="text" placeholder="名"
-            required name="mei" class="half_txt">
-    </div>
-    <div  class="item">
-        <p class="sub_title">　ふりがな　　<span  class="must">必須</span></p>
-        <input type="text" placeholder="せい"  required name="fSei" class="half_txt">　<input type="text" placeholder="めい"
-             required name="fMei" class="half_txt">
-    </div>
-    <div  class="item">
-        <p class="sub_title">　電話番号(ハイフン無し)　　<span class="must">必須</span></p>
-        <input type="text" placeholder=""  required name="tel" class="txt">
-    </div>
-    <div  class="item">
-        <p class="sub_title">　住所　　<span  class="must">必須</span></p>
-        {{--  <input type="text" placeholder=""  required name="address" class="txt">  --}}
-        <select name="prefecture_id" size="1" required  placeholder="都道府県"  class="pre_txt">
-            @foreach ($prefecture as $pre)
-            <option value="{{$pre->id}}">{{$pre->prefecture}}</option>
-            @endforeach
-            </select>　<input type="text" placeholder="市区町村"required name="municipality" class="onethird_txt">　 <input type="text" placeholder="番地" required name="house_number" class="onethird_txt">
-    </div>
-    <div  class="item">
-        <p class="sub_title">　メールアドレス　　<span class="must">必須</span></p>
-        <input type="text" placeholder=""required name="email" class="txt">
-    </div>
-    <div  class="btn">
-        <button type="submit" class="submit_btn">送信</button>
-    </div>
+        {{-- name属性つける --}}
+        <form action="{{ route('member.add') }}" method="POST">
+            @csrf
+            <div class="item">
+                <p class="sub_title">　名前　　<span class="must">必須</span></p>
+                <input type="text" placeholder="姓"required name="sei" class="half_txt" pattern="^[^ 　0-9\$/_\[\]\t]+$">　<input type="text"
+                    placeholder="名" required name="mei" class="half_txt" pattern="^[^ 　0-9\$/_\[\]\t]+$">
+            </div>
+            <div class="item">
+                <p class="sub_title">　ふりがな　　<span class="must">必須</span></p>
+                <input type="text" placeholder="せい" required name="fSei" class="half_txt" pattern="^[ぁ-ん]{1,30}+$|^[ァ-ヶ]{1,30}+$">　<input type="text"
+                    placeholder="めい" required name="fMei" class="half_txt" pattern="^[ぁ-ん]{1,30}+$|^[ァ-ヶ]{1,30}+$">
+            </div>
+            <div class="item">
+                <p class="sub_title">　電話番号(ハイフン無し)　　<span class="must">必須</span></p>
+                <input type="text" placeholder="" required name="tel" class="txt" pattern="^[0-9]{8,14}+$">
+            </div>
+            <div class="item">
+                <p class="sub_title">　住所　　<span class="must">必須</span></p>
+                {{--  <input type="text" placeholder=""  required name="address" class="txt">  --}}
+                <select name="prefecture_id" size="1" required placeholder="都道府県" class="pre_txt">
+                    @foreach ($prefecture as $pre)
+                        <option value="{{ $pre->id }}">{{ $pre->prefecture }}</option>
+                    @endforeach
+                </select>　<input type="text" placeholder="市区町村"required name="municipality" class="onethird_txt" pattern="^[^ 　\$/_\!\?！？]+[ 　]{0,10}+[^ 　\$/_\!\?！？]+$">
+                <input type="text" placeholder="番地" required name="house_number" class="onethird_txt" pattern="^[^ 　\$/_\!\?！？]+[ 　]{0,10}+[^ 　\$/_\!\?！？]+$">
+            </div>
+            <div class="item">
+                <p class="sub_title">　メールアドレス　　<span class="must">必須</span></p>
+                <input type="text" placeholder=""required name="email" class="txt" pattern="^[^ 　\t@＠]+@+[^ 　@＠]+$">
+            </div>
+            <div class="btn">
+                <button type="submit" class="submit_btn">送信</button>
+            </div>
 
-    <input type="hidden" id="user" value="" name="userId">
-    <input type="hidden"  value="{{$store_id}}" name="store_id">
-</form>
-</div>
+            <input type="hidden" id="user" value="" name="userId">
+            <input type="hidden" value="{{ $store_id }}" name="store_id">
+        </form>
+    </div>
 
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
-    <script src="{{secure_asset('js/jquery-1.9.0.min.js')}}" type="text/javascript"></script>
+    <script src="{{ secure_asset('js/jquery-1.9.0.min.js') }}" type="text/javascript"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="{{secure_asset('js/devgetuserId.js')}}"></script>
+    <script src="{{ secure_asset('js/devgetuserId.js') }}"></script>
 </body>
 
 </html>
