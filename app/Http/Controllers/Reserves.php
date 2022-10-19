d25ad1fba4d436f938f03f612fb03ec2050618b9
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-19 15:11:16 +0900

    ｓ

--- app/Http/Controllers/Reserves.php
+++ app/Http/Controllers/Reserves.php
@@ -87,7 +87,7 @@ class Reserves extends Controller
         if (strpos($http_response_header[0], '200') === false) {
             error_log('Request failed: ' . $response);
         }
-        dd($response);
+        dd($request->credit_token);
 
     //     $api_url ='https://api.pay.jp/v1/charges?'.$param;
 
