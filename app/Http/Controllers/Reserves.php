9cd45cbcf955547b0327c702182cfc59447dc8d4
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-19 16:26:35 +0900

    s

--- app/Http/Controllers/Reserves.php
+++ app/Http/Controllers/Reserves.php
@@ -56,8 +56,9 @@ class Reserves extends Controller
         $request->credit_token;
 
         //Authorization　に、秘密鍵を渡す
+        $pass =base64_encode('tribe0819');
         $header = array(
-            'Authorization: Basic sk_test_e7c71bc57ca67b1092849ac7:tribe0819',  
+            'Authorization: Basic sk_test_e7c71bc57ca67b1092849ac7:'.$pass,  
              'Content-type: application/x-www-form-urlencoded'
         );
 
