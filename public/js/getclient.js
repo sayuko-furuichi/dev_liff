79ed2352138d690e30ef23ccf0a3e112b0d10d4b
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-20 17:59:01 +0900

    a

--- public/js/getclient.js
+++ public/js/getclient.js
@@ -2,6 +2,7 @@ window.onload = function () {
 	const defaultLiffId = '1657487333-JPeEg6lr';   // change the default LIFF value if you are not using a node server
 	liffInit(defaultLiffId);
 
+	valid();
 	//公開鍵を渡す
 	const payjp = Payjp('pk_test_9f39566bd5b48e9d0334cbd2');
 	// 大元のElementsインスタンスを生成する
@@ -101,7 +102,15 @@ window.onload = function () {
 
 	}
 
+	function valid(){
+		var valid =document.getElementById('token2').innerText;
 
+		if (valid != '照会が完了しました'){
+			return true;
+		}else{
+			return false;
+		}
+	}
 
 }
 
