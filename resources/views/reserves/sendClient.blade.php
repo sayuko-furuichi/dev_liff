f2647cbf5dfea324a40a0c6c4639081132343a36
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-20 17:03:50 +0900

    s

--- resources/views/reserves/sendClient.blade.php
+++ resources/views/reserves/sendClient.blade.php
@@ -55,9 +55,9 @@
     <p>カード名義氏名：</p>
     <input type="text" name="credit_name" id="credit_name" required class="holder_nm" placeholder="NAGETA REIGAI">
     {{--  <button >カードを照会する</button>  --}}
-    <p id="token2" class="must"></p>
+    <span id="token2" class="must"></span>
     <input type="hidden" id="credit_token" name="credit_token" value="">
-    <button class="btn" id="create_tkn">照会する</button>
+    <button class="btn" id="create_tkn" form="none">照会する</button>
     @endif
     {{--  <p><input type="checkbox" name="member"> 追加情報を入力して会員登録する</p>  --}}
 </div>
