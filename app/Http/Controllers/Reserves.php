fa308e66a7a21730478d17b3caeb0cd54e53be06
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-20 10:15:37 +0900

    とりあえずエラー時の文も追加

--- app/Http/Controllers/Reserves.php
+++ app/Http/Controllers/Reserves.php
@@ -10,41 +10,42 @@ class Reserves extends Controller
     private $storeId;
     private $user_id;
 
-    function __construct(Request $request){
+    public function __construct(Request $request)
+    {
         $this->storeId =$request->store;
-}
+    }
     //全部getして、最後予約の段階でPOSTして保存はどう？
-    function menu(Request $request){
-
-        return view('reserves.menu',[
+    public function menu(Request $request)
+    {
+        return view('reserves.menu', [
             'request'=>$request
         ]);
     }
-    function date(Request $request){
-        return view('reserves.dateTime',[
+    public function date(Request $request)
+    {
+        return view('reserves.dateTime', [
             'request'=>$request
         ]);
-       
     }
-    function confirm(Request $request){
-      
-        return view('reserves.confirm',[
+    public function confirm(Request $request)
+    {
+        return view('reserves.confirm', [
             'request'=>$request
         ]);
-        
-       
     }
-    function send(Request $request){
+    public function send(Request $request)
+    {
         $creditForm=0;
-        if($request->pay=='kureka'){
+        if ($request->pay=='kureka') {
             $creditForm=1;
         }
-        return view('reserves.sendClient',[
+        return view('reserves.sendClient', [
             'request'=>$request,
             'credit'=>$creditForm
         ]);
     }
-    function submit(Request $request){
+    public function submit(Request $request)
+    {
         // $request->store;
         // $request->dateTime;
         // $request->courses[0];
@@ -64,34 +65,39 @@ class Reserves extends Controller
 
         ];
         //配列を hoge=hoge& のHTTPクエリパラメータにする
-         $param=http_build_query($param, "", "&");
+        $param=http_build_query($param, "", "&");
 
 
         $api_url ='https://api.pay.jp/v1/charges';
 
         //headerをsetする。authoriは付けない。POSTするので指定のtypeで
         $headers = array('Content-type: application/x-www-form-urlencoded');
-        
+
 
         $curl_handle = curl_init();
-        
+
         //POST送信する
-         curl_setopt($curl_handle, CURLOPT_POST, true);
+        curl_setopt($curl_handle, CURLOPT_POST, true);
         curl_setopt($curl_handle, CURLOPT_URL, $api_url);
-         curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $headers);
-    
-         curl_setopt($curl_handle, CURLOPT_POSTFIELDS,$param);
+        curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $headers);
+
+        curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $param);
         // curl_exec()の結果を文字列にする
         curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
         //オプションとして、ユーザ名:パスワードを付ける
         curl_setopt($curl_handle, CURLOPT_USERPWD, 'sk_test_e7c71bc57ca67b1092849ac7:');
         //実行
         $res = curl_exec($curl_handle);
-    
+
         //close
         curl_close($curl_handle);
-    
-        return view('reserves.submit',[
+        if (strpos($http_response_header[0], '200') === false) {
+            $res= '決済に失敗しました　';
+        }else{
+            $res+='決済が完了しました！　';
+        }
+
+        return view('reserves.submit', [
             'response'=>$res
         ]);
     }
