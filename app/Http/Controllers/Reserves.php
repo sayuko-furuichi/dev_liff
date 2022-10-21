9f38737a63787bc79093dfe5900e2162f7ac5104
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-21 14:44:18 +0900

    ss

--- app/Http/Controllers/Reserves.php
+++ app/Http/Controllers/Reserves.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Models\Reserve;
 use App\Models\ClientCharge;
 
-
 class Reserves extends Controller
 {
     private $storeId;
@@ -55,14 +54,14 @@ class Reserves extends Controller
 
         if (isset($request->credit_token)) {
             $charges= $this->createCharge($request);
-        
+
 
             return view('reserves.submit', [
                 'response'=>$charges['message'],
-                'charge'=> $charges['charge']
+                'charge'=> $charges['charge'],
+                'store_id'=>$request->store
             ]);
         } else {
-            
             return view('reserves.submit');
         }
     }
@@ -152,49 +151,80 @@ public function createCharge($request)
 }
 
 
-    function cancel(Request $request)
+    public function cancel(Request $request)
     {
         $request->charge_id;
 
         $param =[
             'amount'=>2980,
             'refund_reason' => '予約キャンセルの為'
-      
+
           ];
-          //配列を hoge=hoge& のHTTPクエリパラメータにする
-          $param = http_build_query($param, "", "&");
-      
-      
-          $api_url ='https://api.pay.jp/v1/charges/'. $request->charge_id.'/refund';
-      
-          //headerをsetする。authoriは付けない。POSTするので指定のtypeで
-          $headers = array('Content-type: application/x-www-form-urlencoded');
-      
-      
-          $curl_handle = curl_init();
-      
-          //POST送信する
-          curl_setopt($curl_handle, CURLOPT_POST, true);
-          curl_setopt($curl_handle, CURLOPT_URL, $api_url);
-          curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $headers);
-      
-          curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $param);
-          // curl_exec()の結果を文字列にする
-          curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
-          //オプションとして、ユーザ名:パスワードを付ける
-          curl_setopt($curl_handle, CURLOPT_USERPWD, 'sk_test_e7c71bc57ca67b1092849ac7:');
-          //実行
-          $res = curl_exec($curl_handle);
-      
-          //close
-          curl_close($curl_handle);
-      
-          $charge=json_decode($res, true);
+        //配列を hoge=hoge& のHTTPクエリパラメータにする
+        $param = http_build_query($param, "", "&");
+
+
+        $api_url ='https://api.pay.jp/v1/charges/'. $request->charge_id.'/refund';
+
+        //headerをsetする。authoriは付けない。POSTするので指定のtypeで
+        $headers = array('Content-type: application/x-www-form-urlencoded');
+
+
+        $curl_handle = curl_init();
+
+        //POST送信する
+        curl_setopt($curl_handle, CURLOPT_POST, true);
+        curl_setopt($curl_handle, CURLOPT_URL, $api_url);
+        curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $headers);
+
+        curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $param);
+        // curl_exec()の結果を文字列にする
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+        //オプションとして、ユーザ名:パスワードを付ける
+        curl_setopt($curl_handle, CURLOPT_USERPWD, 'sk_test_e7c71bc57ca67b1092849ac7:');
+        //実行
+        $res = curl_exec($curl_handle);
+
+        //close
+        curl_close($curl_handle);
+
+        $charge=json_decode($res, true);
+
+
+        //DBへのInsert
+        $cardOb=$charge['card'];
+
+        $nwCharge =new ClientCharge();
+        $nwCharge->amount= $charge['amount'];
+
+        $nwCharge->store_id= $request->store;
+        $nwCharge->line_user_id= $request->userId;
+
+        //3項演算子で、NULLだった場合は空文字代入する
+        $nwCharge->reserve_id= 0;
+        $nwCharge->expired_at= $charge['expired_at']==null ? null : date('Y-m-d H:i:s', $charge['expired_at']);
+        $nwCharge->captured_at= $charge['captured_at']==null ? null : date('Y-m-d H:i:s', $charge['captured_at']);
+        $nwCharge->customer_id= $cardOb['customer']==null ? '' : $cardOb['customer'];
+        $nwCharge->description= $charge['description']==null ? '' : $charge['description'];
+
+        $nwCharge->refunded= $charge['refunded']==null ? 0 : $charge['refunded'];
+        $nwCharge->amount_refunded= $charge['amount_refunded']==null ? 0 : $charge['amount_refunded'];
+        $nwCharge->refund_reason= $charge['refund_reason']==null ? '' : $charge['refund_reason'];
+        $nwCharge->fee_rate= $charge['fee_rate']==null ? '' : $charge['fee_rate'];
+        $nwCharge->failure_message= $charge['failure_message']==null ? '' : $charge['failure_message'];
+
+        $nwCharge->charge_id= $charge['id']==null ? '' : $charge['id'];
+        $nwCharge->captured= $charge['captured']==null ? 0 : $charge['captured'];
+        $nwCharge->holder_name= $cardOb['name']==null ? 0 : $cardOb['name'];
+
+        $nwCharge->save();
+
+        $message ="キャンセルを受け付けました。
+                   \nお支払い金額:" .$charge['amount'].
+                  "\n返金額:".$charge['amount_refunded'];
 
         return view('reserves.submit', [
-            'response'=>$res 
+            'response'=>$res
         ]);
     }
-
 }
-    
