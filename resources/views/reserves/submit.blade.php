6bed6a691f15b612f27b241f3eba2c4826016ed2
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-21 14:50:11 +0900

    ss

--- resources/views/reserves/submit.blade.php
+++ resources/views/reserves/submit.blade.php
@@ -30,6 +30,7 @@
 
        <td><img src="{{secure_asset('img/1.png')}}" alt="ロゴマーク" width="25%"><p>まる屋</p></td>
     </div>
+    <input type="hidden" value="{{$store}}" name="store">
     <button type="submit">予約をキャンセルする</button>
 </form>
 </div>
